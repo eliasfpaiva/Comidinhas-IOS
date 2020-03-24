141dd6a3ac9e21c1b0fc9b50c8a7da40b53c9167
elias.paiva <562512Git>
2020-03-24 15:27:24 -0300

    Criação do playground, para prática, agora entendi como funcioan.

---
+++ Comidinhas/MyPlayground.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+//let nome = "churros"
+//let felicidade: Int = 5
+//let calorias: Double = 79.5
+//let vegetal: Bool = false
+//
+////func alimentoconsumido() {
+////    print("O alimento consumido foi: \(nome)")
+////}
+////
+////alimentoconsumido()
+////alimentoconsumido()
+////
+////func alimentoconsumido(nome: String, calorias: Double) {
+////    print("O alimento consumido foi: \(nome), com \(calorias) calorias")
+////}
+////
+////alimentoconsumido(nome: nome, calorias: calorias)
+//
+///*
+//    colocando o underLine antes do nome do parâmentro
+//    não precia reescrever o nome para chamar o método
+//*/
+//func alimentoconsumido(_ nome: String, _ calorias: Double) {
+//    print("O alimento consumido foi: \(nome), com \(calorias) calorias")
+//}
+//
+//alimentoconsumido(nome, calorias)
+
+//let caloria1 = 50.5
+//let caloria2 = 100
+//let caloria3 = 300
+//let caloria4 = 500
+//
+//let totalDeCalorias = [50.5, 100, 300, 500]
+
+//for i in 0...3{
+//    print("\(i): \(totalDeCalorias[i])")
+//}
+
+//for i in 0...totalDeCalorias.count - 1{
+//    print("\(i): \(totalDeCalorias[i])")
+//}
+//for i in totalDeCalorias{
+//    print("\(i)")
+//}
+
+//func soma(valores:[Double]) -> Double {
+//    var soma : Double = 0
+//
+//    for item in valores {
+//        soma += item
+//    }
+//
+//    return soma
+//}
+//
+//print(soma(valores: totalDeCalorias))
